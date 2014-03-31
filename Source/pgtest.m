4828778a513c1c733e24af0bd5c3db8f2ee6906a
Aaron Burghardt <aburgh@mac.com>
2014-03-31 09:10:26 -0400

    Clean-up pgtest.m and exercise fast enumeration and subscripting.

--- Source/pgtest.m
+++ Source/pgtest.m
@@ -10,66 +10,99 @@
 #import <PGCocoa/PGConnection.h>
 #import <PGCocoa/PGResult.h>
 #import <PGCocoa/PGPreparedQuery.h>
+#import <PGCocoa/PGRow.h>
+#import <err.h>
+#import <errno.h>
 #import <syslog.h>
 
 void simpleTest(PGConnection *conn)
 {
 	//	PGResult *result = [conn executeQuery:@"select * from pg_tablespace;"];
-	PGResult *result = [conn executeQuery:@"select * from abperson"];
-	
-	printf("Number of rows: %li\n", [result numberOfRows]);
-	printf("Number of cols: %li\n", [result numberOfFields]);
+	PGResult *result = [conn executeQuery:@"SELECT * FROM person"];
+
+	printf("%s:\n", __func__);
+	printf("\tNumber of rows: %li\n", result.numberOfRows);
+	printf("\tNumber of cols: %li\n", result.numberOfFields);
 	
 	for (int i = 0; i < [result numberOfRows]; i++) {
 		for (int j = 0; j < [result numberOfFields]; j++) {
-			printf("Row: %i col: %i value: %s\n", i, j, [[[result valueAtRowIndex:i fieldIndex:j] description] cStringUsingEncoding:NSUTF8StringEncoding]);
+			printf("\tRow: %i col: %i value: %s\n", i, j, [[[result valueAtRowIndex:i fieldIndex:j] description] cStringUsingEncoding:NSUTF8StringEncoding]);
 		}
 	}
 }
 
+// ****************
+
+static NSString * qry_test1 = @"insert into testnums values ($1, $2, $3, $4, $5);";
+
 void test1(PGConnection *conn)
 {
-	NSArray *values = @[[NSDate date], [NSDate date], [NSNumber numberWithFloat:98.62], [NSNumber numberWithDouble:10023445.98373], [@"some bytes" dataUsingEncoding:NSUTF8StringEncoding]];
-	PGQueryParameters *params = [PGQueryParameters queryParametersWithValues:values];
+	NSDate *now;
+	NSData *data;
+	NSArray *values;
+	PGResult *result;
+	PGQueryParameters *params;
 
-	PGResult *result = [conn executeQuery:@"insert into testnums values ($1, $2, $3, $4, $5);" parameters:params];
+	now = [NSDate date];
+	data = [@"some bytes" dataUsingEncoding:NSUTF8StringEncoding];
+	values = @[now, now, @(98.62f), @(10023445.98373), data];
+	params = [PGQueryParameters queryParametersWithValues:values];
+	result = [conn executeQuery:qry_test1 parameters:params];
 
-	if (result.status != kPGResultTuplesOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+	if (result.status != kPGResultCommandOK)
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 }
+
+
+// ****************
+
+static NSString *qry_test2 = @"INSERT INTO person(id, first, last) VALUES($1, $2, $3);";
 
 void test2(PGConnection *conn)
 {
-	NSArray *values = @[[NSNumber numberWithInt:4], @"Veryl", @"Burghardt"];
-	PGQueryParameters *params = [PGQueryParameters queryParametersWithValues:values];
+	NSArray *values;
+	PGQueryParameters *params;
+	PGResult *result;
 
-	PGResult *result = [conn executeQuery:@"insert into abperson(rowid, first, last) values( $1, $2, $3);" parameters:params];
+	values = @[@(4), @"John", @"Doe"];
+	params = [PGQueryParameters queryParametersWithValues:values];
+	result = [conn executeQuery:qry_test2 parameters:params];
 
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 }
+
+// ****************
+
+static NSString *qry_test3 = @"insert into testnums (f2) values ($1);";
 
 void test3(PGConnection *conn)
 {
-	NSArray *values = @[[NSNumber numberWithDouble:10023445.98373]];
+	NSArray *values = @[ @(10023445.98373) ];
 	PGQueryParameters *params = [PGQueryParameters queryParametersWithValues:values];
 
-	PGResult *result = [conn executeQuery:@"insert into testnums (f2) values ($1);" parameters:params];
+	PGResult *result = [conn executeQuery:qry_test3 parameters:params];
 
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 }
+
+// ****************
+
+static NSString *qry_test4 = @"insert into testnums (data) values ($1);";
 
 void test4(PGConnection *conn)
 {
 	NSArray *values = @[[@"Some sample data" dataUsingEncoding:NSUTF8StringEncoding]];
 	PGQueryParameters *params = [PGQueryParameters queryParametersWithValues:values];
 
-	PGResult *result = [conn executeQuery:@"insert into testnums (data) values ($1);" parameters:params];
+	PGResult *result = [conn executeQuery:qry_test4 parameters:params];
 
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 }
+
+// ****************
 
 void test5(PGConnection *conn)
 {
@@ -101,27 +134,27 @@ void test5(PGConnection *conn)
 
 	PGResult *result = [query executeWithParameters:params];
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
-	
-	[params bindValue:[NSDate date] atIndex:1];
-	[params bindValue:[NSNumber numberWithDouble:0.0] atIndex:3];
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
+
+	params[1] = [NSDate date];
+	params[3] = @(0.0);
 	result = [query executeWithParameters:params];
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 	sleep(1);
 
-	[params bindValue:[NSDate date] atIndex:1];
-	[params bindValue:[NSNumber numberWithDouble:1.0] atIndex:3];
+	params[1] = [NSDate date];
+	params[3] = @(1.0);
 	result = [query executeWithParameters:params];
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 	sleep(2);
 
-	[params bindValue:[NSDate date] atIndex:1];
-	[params bindValue:[NSNumber numberWithDouble:2.0] atIndex:3];
+	params[1] = [NSDate date];
+	params[3] =@(2.0);
 	result = [query executeWithParameters:params];
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 
 	if ([conn commitTransaction] == NO) {
 		syslog(LOG_DEBUG, "commit transaction: %s", conn.errorMessage.UTF8String);
@@ -130,58 +163,126 @@ void test5(PGConnection *conn)
 	};
 
 	if (result.status != kPGResultCommandOK)
-		printf("Result: %s\n", result.error.description.UTF8String);
+		warnx("%s: result: %s\n", __func__, result.error.description.UTF8String);
 }
 
 void test6(PGConnection *conn)
 {
 	PGResult *result = [conn executeQuery:@"SELECT * FROM testnums;"];
-	NSLog(@"Headers: %@", [[result fieldNames] componentsJoinedByString:@", "]);
-	printf("Result: %s\n", [[[result error] description] UTF8String]);
+	printf("Headers: %s\n", [[result.fieldNames componentsJoinedByString:@", "] UTF8String]);
+	printf("Result:  %s\n", result.error.description.UTF8String);
 }
 
 void test7(PGConnection *conn)
 {
-	PGResult *result = [conn executeQuery:@"SELECT * FROM testnums;" parameters:[NSArray array]];
+	PGResult *result = [conn executeQuery:@"SELECT * FROM testnums;" parameters:nil];
+
+	printf("%s:\n", __func__);
+	printf("\t%s\n", [[result.fieldNames componentsJoinedByString:@"\t"] UTF8String]);
 	
-	printf("%s\n", [[[result fieldNames] componentsJoinedByString:@"\t"] UTF8String]);
-	
-	for (int i = 0; i < [result numberOfRows]; i++) {
-		for (int j = 0; j < [result numberOfFields]; j++) 
+	for (int i = 0; i < result.numberOfRows; i++) {
+		putchar('\t');
+		for (int j = 0; j < result.numberOfFields; j++)
 			printf("%s\t", [[[result valueAtRowIndex:i fieldIndex:j] description] UTF8String]);
+		putchar('\n');
+	}
+}
+
+void test8(PGConnection *conn)
+{
+	PGResult *result = [conn executeQuery:@"SELECT * FROM testnums;" parameters:nil];
+	PGRow *row;
+	NSObject *field;
+
+	printf("%s:\n", __func__);
+	printf("\t%s\n", [[result.fieldNames componentsJoinedByString:@"\t"] UTF8String]);
+
+//	for (int i = 0; i < result.numberOfRows; i++) {
+//		row = result[i];
+//		for (int j = 0; j < result.numberOfFields; j++)
+//			printf("%s\t", [[row[j] description] UTF8String]);
+//		printf("\n");
+//	}
+	for (row in result) {
+		for (field in row)
+			printf("\t%s\t", field.description.UTF8String);
 		printf("\n");
 	}
 }
-			
+
+void DropTable(PGConnection *conn, NSString *name)
+{
+	PGResult *result;
+	NSString *qry;
+
+	qry = [NSString stringWithFormat:@"DROP TABLE %@;", name];
+
+	result = [conn executeQuery:qry];
+	if (result.status != kPGResultCommandOK)
+		warnx("drop table '%s': %s", name.UTF8String, result.error.description.UTF8String);
+}
+
+static NSString *table_testnums = @"CREATE TABLE testnums (d1 TIMESTAMP, d2 TIMESTAMPTZ, f1 FLOAT, f2 DOUBLE PRECISION, data BYTEA);";
+static NSString *table_person = @"CREATE TABLE person (id SERIAL PRIMARY KEY, first TEXT, last TEXT);";
 
 int main(int argc, char *argv[]) 
 {
 	openlog("pgtest",  LOG_PERROR | LOG_CONS, LOG_USER);
-	
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	
-	NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:@"test", PGConnectionParameterDatabaseNameKey, nil];
-	PGConnection *conn = [[PGConnection alloc] initWithParameters:params];
-	
-	if (![conn connect]) {
-		syslog(LOG_ERR, "connect: %s", conn.errorMessage.UTF8String);
-		goto bail;
-	}
 
-//	simpleTest(conn);
-//	test1(conn);
-//	test2(conn);
-//	test3(conn);
-//	test4(conn);
-	test5(conn);
-//	test6(conn);
-//	test7(conn);
-	
-//	[conn close];
-//	[conn release];
-	
+	@autoreleasepool {
+		PGConnection *conn;
+		NSDictionary *params;
+		NSArray *values;
+		NSData *data;
+
+		params = @{ PGConnectionParameterDatabaseNameKey:@"test", PGConnectionParameterHostKey:@"/tmp" };
+		conn = [[PGConnection alloc] initWithParameters:params];
+
+		if (![conn connect]) {
+			syslog(LOG_ERR, "connect: %s", conn.errorMessage.UTF8String);
+			goto bail;
+		}
+
+		[conn beginTransaction];
+//		values = @[[NSDate date], [NSDate date], @(98.62f), @(10023445.98373), [@"some bytes" dataUsingEncoding:NSUTF8StringEncoding]];
+
+		PGResult *result;
+		result = [conn executeQuery:table_testnums];
+		result = [conn executeQuery:table_person];
+
+		simpleTest(conn);
+		putchar('\n');
+
+		test1(conn);
+		putchar('\n');
+
+		test2(conn);
+		putchar('\n');
+
+		test3(conn);
+		putchar('\n');
+
+		test4(conn);
+		putchar('\n');
+
+		test5(conn);
+		putchar('\n');
+
+		test6(conn);
+		putchar('\n');
+
+		test7(conn);
+		putchar('\n');
+
+		test8(conn);
+		putchar('\n');
 bail:
-	[pool release];
+		DropTable(conn, @"testnums");
+		DropTable(conn, @"person");
+		[conn rollbackTransaction];
+		[conn disconnect];
+		[conn release];
+	}
 
 	return 0;
 }
